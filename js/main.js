696aac562c48556470195205dfc363e67a0e4dbd
Evgeniya Mironova <glandea@icloud.com>
2018-08-09 00:57:37 +0300

    добавь правки и комментарии

--- js/main.js
+++ js/main.js
@@ -1,49 +1,75 @@
-  const greeting = document.getElementById(`greeting`);
-  const rules = document.getElementById(`rules`);
-  const intro = document.getElementById(`intro`);
-  const game1 = document.getElementById(`game-1`);
-  const game2 = document.getElementById(`game-2`);
-  const game3 = document.getElementById(`game-3`);
-  const stats = document.getElementById(`stats`);
-  const modalError = document.getElementById(`modal-error`);
-  const modalConfirm = document.getElementById(`modal-confirm`);
-  const arrowsBtn = document.querySelectorAll(`.arrows__btn`);
+//константы  для правой и левой клавиш
+const RIGHT_ARROW_BTN = 39;
+const LEFT_ARROW_BTN = 37;
 
-  const arrayScreen = [rules, greeting, intro, game1, game2, game3, stats, modalError, modalConfirm];
+//создаем массив экранов
+const screens = Array.from(document.querySelectorAll(`template`)).map(it => it.content);
 
-  const mainElement = document.querySelector(`#main`);
-  mainElement.appendChild(game3.content);
+//показываем экран
+const mainElement = document.querySelector(`#main`);
 
-  const showScreen = element => {
-   mainElement.innerHTML = ``;
-   mainElement.appendChild(element.content.cloneNode(true));
-  };
+const showScreen = element => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element.cloneNode(true));
+};
 
-  let current = 0;
-  const selectScreenNumber = (index) => {
-   index = index < 0 ? arrayScreen.length - 1 : index;
-   index = index >= arrayScreen.length ? 0 : index;
-   current = index;
-   showScreen(arrayScreen[current]);
+//листаем
+let current = 0;
+const selectScreenNumber = (index) => {
+  index = index < 0 ? 0 : index;
+  index = index >= screens.length ? screen.length - 1 : index;
+  current = index;
+  showScreen(screens[current]);
+}
+
+selectScreenNumber(1);
+
+//добавляем htlm-стрелки
+const block = document.createElement(`div`);
+
+const createButtons = () => {
+  block.className = "arrows__wrap";
+  block.innerHTML = `
+    <style>
+      .arrows__wrap {
+        position: absolute;
+        top: 95px;
+        left: 50%;
+        margin-left: -56px;
+      }
+      .arrows__btn {
+        background: none;
+        border: 2px solid black;
+        padding: 5px 20px;
+      }
+    </style>
+    <button class="arrows__btn"><-</button>
+    <button class="arrows__btn">-></button>
+  `
+};
+
+document.querySelector(`body`).appendChild(block);
+createButtons();
+
+
+//вешаем событие с клавиатуры на стрелки
+document.addEventListener(`keydown`, (evt) => {
+  switch (evt.keyCode) {
+    case LEFT_ARROW_BTN:
+    selectScreenNumber(current + 1);
+    break;
+    case RIGHT_ARROW_BTN:
+    selectScreenNumber(current - 1);
+    break;
   }
+});
 
-  selectScreenNumber(0);
+//вешаем событие с мыши на стрелки
+const arrowsBtn = document.querySelectorAll(`.arrows__btn`);
+arrowsBtn[0].addEventListener(`click`, (event) => {
+  selectScreenNumber(current - 1);
+});
 
-  document.addEventListener(`keydown`, (evt) => {
-    switch (evt.keyCode) {
-      case 37:
-      selectScreenNumber(current + 1);
-      break;
-      case 39:
-      selectScreenNumber(current - 1);
-      break;
-    }
-  });
-
-  arrowsBtn[0].onclick = () => {
-    selectScreenNumber(current - 1)
-  };
-
-  arrowsBtn[1].onclick = () => {
-    selectScreenNumber(current + 1)
-  };
+arrowsBtn[1].addEventListener(`click`, (event) => {
+  selectScreenNumber(current + 1);
+});
