121486e89ecce710dda984f76c140773f4e254c0
Evgeniya Mironova <glandea@icloud.com>
2018-08-09 17:29:13 +0300

    исправь мелкие косяки

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
-//константы  для правой и левой клавиш
+'use strict';
+
+// константы  для правой и левой клавиш
 const RIGHT_ARROW_BTN = 39;
 const LEFT_ARROW_BTN = 37;
 
-//константа для приветственного экрана
-const loadScreen = 1;
+// константа для приветственного экрана
+const LOADSCREEN = 1;
 
-//создаем массив экранов
-const screens = Array.from(document.querySelectorAll(`template`)).map(it => it.content);
+// создаем массив экранов
+const screens = Array.from(document.querySelectorAll(`template`)).map((it) => it.content);
 
-//показываем экран
+// показываем экран
 const mainElement = document.querySelector(`#main`);
 
-const showScreen = element => {
+const showScreen = (element) => {
   mainElement.innerHTML = ``;
   mainElement.appendChild(element.cloneNode(true));
 };
 
-//листаем
+// листаем
 let current = 0;
 const selectScreenNumber = (index) => {
   index = index < 0 ? 0 : index;
   index = index >= screens.length ? screen.length - 1 : index;
   current = index;
   showScreen(screens[current]);
-}
+};
 
-selectScreenNumber(loadScreen);
+selectScreenNumber(LOADSCREEN);
 
-//добавляем htlm-стрелки
+
+// добавляем htlm-стрелки
 const block = document.createElement(`div`);
 
 const createButtons = () => {
-  block.className = "arrows__wrap";
+  block.className = `arrows__wrap`;
   block.innerHTML = `
     <style>
       .arrows__wrap {
@@ -48,31 +51,31 @@ const createButtons = () => {
     </style>
     <button class="arrows__btn"><-</button>
     <button class="arrows__btn">-></button>
-  `
+  `;
 };
 
 document.querySelector(`body`).appendChild(block);
 createButtons();
 
+// вешаем событие с клавиатуры на стрелки
 
-//вешаем событие с клавиатуры на стрелки
 document.addEventListener(`keydown`, (evt) => {
   switch (evt.keyCode) {
     case LEFT_ARROW_BTN:
-    selectScreenNumber(current + 1);
-    break;
+      selectScreenNumber(current + 1);
+      break;
     case RIGHT_ARROW_BTN:
-    selectScreenNumber(current - 1);
-    break;
+      selectScreenNumber(current - 1);
+      break;
   }
 });
 
-//вешаем событие с мыши на стрелки
+// вешаем событие с мыши на стрелки
 const arrowsBtn = document.querySelectorAll(`.arrows__btn`);
-arrowsBtn[0].addEventListener(`click`, (event) => {
+arrowsBtn[0].addEventListener(`click`, () => {
   selectScreenNumber(current - 1);
 });
 
-arrowsBtn[1].addEventListener(`click`, (event) => {
+arrowsBtn[1].addEventListener(`click`, () => {
   selectScreenNumber(current + 1);
 });
