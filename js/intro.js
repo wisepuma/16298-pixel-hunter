d7ec3e769d614e94e8428cd1237c6def4abee8c5
Evgeniya Mironova <glandea@icloud.com>
2018-08-16 01:46:01 +0300

    разбей код на модули и свяжи их

---
+++ js/intro.js
@@ -0,0 +1,20 @@
+import {changeScreen, render} from './util';
+import greeting from './greeting';
+
+const template = `<section class="intro">
+<button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+<p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+</section>`;
+
+const element = render(template);
+const continueButton = element.querySelector(`button.intro__asterisk`);
+
+// greeting показывается по нажатию на символ звёздочки
+
+continueButton.addEventListener(`click`, () => {
+  changeScreen(greeting);
+});
+
+export default element;
+
+
---
+++ js/util.js
@@ -0,0 +1,12 @@
+export const render = (template) => {
+  const wrapper = document.createElement(`div`);
+  wrapper.innerHTML = template.trim();
+  return wrapper;
+};
+
+const mainElement = document.querySelector(`#main`);
+
+export const changeScreen = (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element);
+};
