871b98dc4014c7a02e65edd62b43544ea407bf72
Evgeniya Mironova <glandea@icloud.com>
2018-09-10 01:08:35 +0300

    add more tests

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,16 +1,37 @@
 import {assert} from 'chai';
-// импортируем параметры из game-data
-import {answers, calculateScore, lifes} from './game-data';
+import {calculateScore} from './game-data';
+
 mocha.setup(`bdd`);
+
+const answers = [
+  [{answer: true, time: 10}, {answer: true, time: 10}, {answer: true, time: 10}, {answer: true, time: 10}, {answer: true, time: 10}, {answer: true, time: 10}, {answer: true, time: 10}, {answer: true, time: 10}, {answer: true, time: 10}, {answer: true, time: 10}],
+  [{answer: true, time: 20}, {answer: true, time: 20}, {answer: true, time: 15}, {answer: true, time: 15}, {answer: true, time: 15}, {answer: true, time: 11}, {answer: true, time: 11}, {answer: true, time: 15}, {answer: true, time: 20}, {answer: true, time: 20}],
+  [{answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}],
+  [{answer: false, time: 25}, {answer: false, time: 25}, {answer: false, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}, {answer: true, time: 25}],
+  [{answer: false, time: 20}, {answer: false, time: 20}, {answer: true, time: 15}, {answer: true, time: 15}, {answer: true, time: 15}, {answer: true, time: 11}, {answer: true, time: 11}, {answer: true, time: 15}, {answer: true, time: 20}, {answer: true, time: 20}],
+];
 
 describe(`correct results`, () => {
   it(`true answers should return 1150`, () => {
-    if (calculateScore(answers, 3) !== 1150) {
-      throw new Error(`ALL IS DUST`);
-    }
+    assert.equal(calculateScore(answers[1], 3), 1150);
+  });
+
+  it(`fast answers should return 1650`, () => {
+    assert.equal(calculateScore(answers[0], 3), 1650);
+  });
+
+  it(`slow answers should return 650`, () => {
+    assert.equal(calculateScore(answers[2], 3), 650);
+  });
+
+  it(`three false answers should return -1`, () => {
+    assert.equal(calculateScore(answers[3], 3), -1);
+  });
+
+  it(`two false answers should return 950`, () => {
+    assert.equal(calculateScore(answers[4], 3), 950);
   });
 
 });
-
 
 mocha.run();
